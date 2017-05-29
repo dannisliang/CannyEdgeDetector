ba269a868ddc169199c2a67c4567666c2268a7c2
liuyi475 <yixuanyo@gmail.com>
2017-05-29 20:16:53 +1200

    Initial commit

---
+++ app/src/main/java/com/altotech/glass/api/edgedetector1/MainActivity.java
@@ -0,0 +1,109 @@
+package com.altotech.glass.api.edgedetector1;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceView;
+import android.widget.TextView;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.JavaCameraView;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+
+public class MainActivity extends Activity implements CameraBridgeViewBase.CvCameraViewListener2{
+
+    // Used to load the 'native-lib' library on application startup.
+    private static String TAG = "MainActivity";
+    JavaCameraView javaCameraView;
+    Mat mRgba;
+    BaseLoaderCallback baseLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status){
+                case BaseLoaderCallback.SUCCESS:{
+                    javaCameraView.enableView();
+                    break;
+                }
+                default:{
+                    super.onManagerConnected(status);
+                    break;
+                }
+
+            }
+            super.onManagerConnected(status);
+        }
+    };
+    static {
+        System.loadLibrary("native-lib");
+        if (OpenCVLoader.initDebug()){
+            Log.i(TAG,"OpenCV loaded successfully");
+        }else{
+            Log.i(TAG, "OpenCV not loaded");
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Example of a call to a native method
+        TextView tv = (TextView) findViewById(R.id.sample_text);
+        tv.setText(stringFromJNI());
+        javaCameraView = (JavaCameraView) findViewById(R.id.java_camera_view);
+        javaCameraView.setVisibility(SurfaceView.VISIBLE);
+        javaCameraView.setCvCameraViewListener(this);
+    }
+    @Override
+    protected void onPause(){
+        super.onPause();
+        if(javaCameraView!=null){
+            javaCameraView.disableView();
+        }
+    }
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        if(javaCameraView!=null){
+            javaCameraView.disableView();
+        }
+    }
+    @Override
+    protected void onResume(){
+        super.onResume();
+        if (OpenCVLoader.initDebug()){
+            Log.i(TAG,"OpenCV loaded successfully");
+            baseLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        }else{
+            Log.i(TAG, "OpenCV not loaded");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_9,this,baseLoaderCallback);
+        }
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+        mRgba=new Mat(height,width, CvType.CV_8UC4);
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+        mRgba.release();
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+       mRgba = inputFrame.rgba();
+        return mRgba;
+    }
+}
